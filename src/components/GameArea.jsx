45bb2ff789dddbd21c776198b044912c829a72b5
Fu94f <fulvia.franco1994@gmail.com>
2026-05-02 18:54:28 +0200

    add game pannel;add style game pannel

--- src/components/GameArea.jsx
+++ src/components/GameArea.jsx
@@ -1,10 +1,17 @@
 import "./GameArea.css"
+import GuessPannel from "./GuessPannel.jsx";
 
 function GameArea(){
     return(
         <div className="game-area">
-            <div className="container">IMAGE</div>
-            <div className="container">MAP</div>
+            <div className="container">
+                <div className="image">IMAGE</div>
+            </div>
+
+            <div className="container right-column">
+                <div className="map">Map</div>
+                <div className="guess-panel"><GuessPannel/></div>
+            </div>
             
         </div>
     );
