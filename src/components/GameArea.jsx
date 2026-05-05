f3ae1e3d09282593e1f35417027cc648f25d51eb
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-05-05 16:08:29 +0200

    Add shuffle array functionality

--- src/components/GameArea.jsx
+++ src/components/GameArea.jsx
@@ -2,16 +2,29 @@ import "./GameArea.css";
 import GuessPannel from "./GuessPannel.jsx";
 import Image from "./Images.jsx";
 import { pics } from "./ImageList.jsx";
+import { useState } from "react";
 
 function GameArea() {
   const handleGuess = () => {
     console.log("Hello", pics[0].city);
   };
+
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    console.log(shuffled);
+    return shuffled;
+  };
+  const [myArray] = useState(() => shuffleArray(pics));
+
   return (
     <div className="game-area">
       <div className="container">
         <div className="image">
-          <Image />
+          <Image images={myArray} />
         </div>
       </div>
 
