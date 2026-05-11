4fd5c8d4144f20cf30f78a1be686ec7e2e73ebe5
Fu94f <fulvia.franco1994@gmail.com>
2026-05-11 19:31:24 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "leaflet/dist/leaflet.css";
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { GameProvider } from "./context/GameContext.jsx";
->>>>>>> 945baa6ece1a524a78ee12d3455fbbc9b3b3dfa2
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
