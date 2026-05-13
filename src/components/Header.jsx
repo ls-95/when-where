a598c35961e5891851a19be020fdb38e5e8fd1c5
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-05-13 11:28:20 +0200

    Add link to logo which navigates to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import "./Header.css";
+import { useNavigate } from "react-router";
 import Logo from "../assets/when-where-logo-white.png";
 
 function Header() {
+  const navigate = useNavigate();
   return (
-    <div className="logo-container">
-      <img src={Logo} alt="logo white" className="logo" />
+    <div className="logo-container" onClick={() => navigate("/home")}>
+      <button className="logo-btn">
+        <img src={Logo} alt="logo white" className="logo" />
+      </button>
     </div>
   );
 }
