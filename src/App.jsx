2310a2dea7de0eb2af1a1b57f9eba19fb704b3fa
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-05-29 14:27:39 +0200

    Add robots.txt. Remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -26,29 +26,3 @@ function App() {
 }
 
 export default App;
-
-// import "./App.css";
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// import LoginPage from "./pages/Loginpage";
-// import RegisterPage from "./pages/Registerpage";
-// import Home from "./pages/Home";
-// import GameScreen from "./pages/GameScreen";
-// import FinalScore from "./pages/FinalScore";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/login" />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/game" element={<GameScreen />} />
-//         <Route path="/score" element={<FinalScore />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
